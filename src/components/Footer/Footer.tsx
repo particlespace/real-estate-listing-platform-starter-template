95d1ea7feb29bb2cd1e39de7fda322ba0956b51f
Isaac Spiegel <isaacspiegel@gmail.com>
2022-06-21 15:01:08 -0500

    Update spacing

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import {
-    Anchor,
-    Text as MantineText,
-    Footer as MantineFooter,
-    Container,
+  Anchor,
+  Text as MantineText,
+  Footer as MantineFooter,
+  Container,
 } from '@mantine/core';
 
 export default function Footer() {
-    return (
-        <MantineFooter
-        height="md"
-        fixed
-        sx={{
-            backgroundColor: '#f4f6f9',
-            borderTop: 'none',
-        }}
-        >
-        <Container size="xl">
-            <MantineText color="dimmed" size="sm">
-            Powered with ♥ by <Anchor variant="link" size="sm" href="https://particlespace.com">Particle Space</Anchor>
-            </MantineText>
-        </Container>
-        </MantineFooter>
-    );
+  return (
+    <MantineFooter
+      height="md"
+      fixed
+      sx={{
+        backgroundColor: '#f4f6f9',
+        borderTop: 'none',
+      }}
+    >
+      <Container size="xl">
+        <MantineText color="dimmed" size="sm">
+          Powered with ♥ by
+          <Anchor variant="link" size="sm" href="https://particlespace.com">
+            Particle Space
+          </Anchor>
+        </MantineText>
+      </Container>
+    </MantineFooter>
+  );
 }
