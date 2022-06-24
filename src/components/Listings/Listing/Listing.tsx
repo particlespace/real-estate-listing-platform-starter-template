1d3608e1d4050660d6927aa5666fabfd22e336ef
mkeleti <mmkeleti@gmail.com>
2022-06-24 11:32:54 -0500

    Initial Commit

--- src/components/Listings/Listing/Listing.tsx
+++ src/components/Listings/Listing/Listing.tsx
@@ -1,14 +1,7 @@
 import {Card, Image, Text, Badge, Button, Group, useMantineTheme} from '@mantine/core';
 
-export interface ListingProps {
-    image: string,
-    title: string,
-    desc: string,
-    sold: boolean,
-    link: string,
-}
 
-export function Listing(ListingProps) {
+export function Listing(props: { image: string, sold: boolean, address: string, price: string | number }) {
     const theme = useMantineTheme();
 
     const secondaryColor = theme.colorScheme === 'dark'
@@ -20,22 +13,22 @@ export function Listing(ListingProps) {
         <div style={{width: 340, margin: 'auto'}}>
             <Card shadow="sm" p="lg">
                 <Card.Section>
-                    <Image src={ListingProps.image} height={160} alt="Norway"/>
+                    <Image src={props.image} height={160} alt="Norway"/>
                 </Card.Section>
 
-                <Group position="apart" style={{ marginBottom: 5, marginTop: theme.spacing.sm }}>
-                    <Text weight={500}>{ListingProps.title}</Text>
-                    <Badge color="pink" variant="light">
-                        {ListingProps.sold ? "On Sale" : "Sold"}
+                <Group position="apart" style={{marginBottom: 5, marginTop: theme.spacing.sm}}>
+                    <Text weight={500}>{props.price}</Text>
+                    <Badge color={props.sold ? "pink" : "green"} variant="light">
+                        {props.sold ? "Sold" : "On Sale"}
                     </Badge>
                 </Group>
 
-                <Text size="sm" style={{ color: secondaryColor, lineHeight: 1.5 }}>
-                    {ListingProps.desc}
+                <Text size="sm" style={{color: secondaryColor, lineHeight: 1.5}}>
+                    {props.address}
                 </Text>
 
-                <Button variant="light" color="blue" ref={ListingProps.link} fullWidth style={{marginTop: 14}}>
-                    Book classic tour now
+                <Button variant="light" color="blue" fullWidth style={{marginTop: 14}}>
+                    More Info
                 </Button>
             </Card>
         </div>
