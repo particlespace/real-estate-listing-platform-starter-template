ed9242dece9c2d9d4766bd44cb3cb93f34e6e4e0
mkeleti <mmkeleti@gmail.com>
2022-06-24 08:47:21 -0500

    Added Listing Props to be used for loading listings in page.

--- src/components/Listings/Listing/Listing.tsx
+++ src/components/Listings/Listing/Listing.tsx
@@ -1,40 +1,40 @@
-import { Card, Image, Text, Badge, Button, Group, useMantineTheme } from '@mantine/core';
+import {Card, Image, Text, Badge, Button, Group, useMantineTheme} from '@mantine/core';
 
-export function Listing() {
+export interface ListingProps {
+    image: string,
+    title: string,
+    desc: string,
+    sold: boolean,
+    link: string,
+}
+
+export function Listing(ListingProps) {
     const theme = useMantineTheme();
 
     const secondaryColor = theme.colorScheme === 'dark'
         ? theme.colors.dark[1]
         : theme.colors.gray[7];
 
-    interface ListingProps {
-        image: string,
-        title: string,
-        desc: string,
-        sold: boolean,
-        link: string,
-    }
 
     return (
-        <div style={{ width: 340, margin: 'auto' }}>
+        <div style={{width: 340, margin: 'auto'}}>
             <Card shadow="sm" p="lg">
                 <Card.Section>
-                    <Image src="./image.png" height={160} alt="Norway" />
+                    <Image src={ListingProps.image} height={160} alt="Norway"/>
                 </Card.Section>
 
                 <Group position="apart" style={{ marginBottom: 5, marginTop: theme.spacing.sm }}>
-                    <Text weight={500}>Norway Fjord Adventures</Text>
+                    <Text weight={500}>{ListingProps.title}</Text>
                     <Badge color="pink" variant="light">
-                        On Sale
+                        {ListingProps.sold ? "On Sale" : "Sold"}
                     </Badge>
                 </Group>
 
                 <Text size="sm" style={{ color: secondaryColor, lineHeight: 1.5 }}>
-                    With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-                    activities on and around the fjords of Norway
+                    {ListingProps.desc}
                 </Text>
 
-                <Button variant="light" color="blue" fullWidth style={{ marginTop: 14 }}>
+                <Button variant="light" color="blue" ref={ListingProps.link} fullWidth style={{marginTop: 14}}>
                     Book classic tour now
                 </Button>
             </Card>
